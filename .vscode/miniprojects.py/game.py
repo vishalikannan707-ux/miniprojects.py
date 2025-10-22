6d82b936a0e5bb5bbf5bd93616d3cf5e7785a975
Vishali Kannan <vishalikannan707@gmail.com>
2025-10-22 11:40:02 +0530

    game.py

---
+++ .vscode/miniprojects.py/game.py
@@ -0,0 +1,24 @@
+import random
+def number_guessing_game():
+    print("Welcome to the Number Guessing Game!!")
+    print("I'm thinking of a number between 1 and 100. ")
+
+    secret_number = random.randint(1,100)
+    attempts=0
+    while True:
+        try:
+            guess=int(input("Enter your guess :"))
+            attempts += 1
+
+            if guess < secret_number:
+                print("Too Low! Try again.")
+            elif guess > secret_number:
+                print("Too high ! Try  again.")
+            else:
+                print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts.")
+                break
+        except ValueError:
+              print("Invalid input. Please enter an integer.")
+    
+if __name__ == "__main__":
+    number_guessing_game()
